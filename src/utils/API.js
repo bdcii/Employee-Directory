4fbf1e640938648dd1db5969287469505a302e98
bdcii <darrell.chess@gmail.com>
2021-06-21 20:23:08 -0400

    added utils

---
+++ src/utils/API.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const apiURL = "https://randomuser.me/api/?results=30&nat=us";
+
+const api = {
+    getEmployees: function() {
+    return axios.get(apiURL);
+}}
+export default api;
